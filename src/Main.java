db021a7641f33fda4cfcbf5d0ce715f18738fb05
Sergey <serjio.nef@gmail.com>
2024-09-07 16:17:13 +0300

    Create Calculator v2.0
    create output.txt
    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         Scanner in = new Scanner(new File("input.txt"));
+        PrintWriter writer = new PrintWriter("output.txt");
 
         try {
             double a = in.nextDouble();
             String operator = in.next();
             double b = in.nextDouble();
+            in.close();
 
             if(!operator.matches("[-|+|*|/]{1}"))
                 throw new Exception("Operation Error!");
             if(operator.equals("/") & b == 0)
                 throw new Exception("Error! Division by zero");
 
-            if(operator.equals("-")) System.out.println(a - b);
-            if(operator.equals("+")) System.out.println(a + b);
-            if(operator.equals("*")) System.out.println(a * b);
-            if(operator.equals("/")) System.out.println(a / b);
+            if(operator.equals("-")) writer.print(a - b);
+            if(operator.equals("+")) writer.print(a + b);
+            if(operator.equals("*")) writer.print(a * b);
+            if(operator.equals("/")) writer.print(a / b);
         }
         catch (InputMismatchException e) {
-            System.out.println("Error! Not number");
+            writer.print("Error! Not number");
         }catch (Exception e) {
-            System.out.println(e.getMessage());
+            writer.print(e.getMessage());
         }
+        writer.close();
 
     }
 }
